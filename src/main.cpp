5f1d975a810bfb85bdd372b5bcf31969893cc8f4
mtorials <git@mtorials.de>
2020-09-22 17:13:39 +0200

    led test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include <Arduino.h>
+#include <FastLED.h>
+
+#include "definements.h"
+#include "test.cpp"
+
+CRGB leds[NUM_LEDS];
+
+void testLEDs(CRGB * leds) {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::White; FastLED.show(); delay(30);
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  FastLED.addLeds<NEOPIXEL, 5>(leds, NUM_LEDS);
+  testLEDs(leds);
 }
 
 void loop() {
