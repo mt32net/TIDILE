76bccdfad37320d7a2bd007dd7d6814a995e40c5
mtorials <git@mtorials.de>
2020-09-22 17:19:14 +0200

    name change

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,21 @@
 #include <FastLED.h>
 
 #include "definements.h"
-#include "test.cpp"
 
 CRGB leds[NUM_LEDS];
 
-void testLEDs(CRGB * leds) {
+void startupLEDs(CRGB * leds) {
   for (int i = 0; i < NUM_LEDS; i++) {
     leds[i] = CRGB::White; FastLED.show(); delay(30);
+  }
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Black; FastLED.show(); delay(30);
   }
 }
 
 void setup() {
   FastLED.addLeds<NEOPIXEL, 5>(leds, NUM_LEDS);
-  testLEDs(leds);
+  startupLEDs(leds);
 }
 
 void loop() {
