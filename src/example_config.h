5c9d1f33c2bd65ecf635f34937087aab2659588a
mtorials <matti.bleckmann@live.com>
2020-09-26 18:34:43 +0200

    touch Sensor smothing

--- src/example_config.h
+++ src/example_config.h
@@ -5,6 +5,10 @@
 #define LED_PIN 5
 #define BLINK_BRIGHTNESS 0.7
 
+// Touch Pin
+#define THRESHOLD 25
+#define SMOOTH_LOOPS 50
+
 #pragma region display settings
 //#define DISPLAY_TEMPERATURE
 //#define DISPLAY_HUMIDIY
