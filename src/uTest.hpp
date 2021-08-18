9695e156d91408d8123ff3e7c30f86508d9e80ce
Universumgames <programming@universegame.de>
2021-08-18 19:16:05 +0200

    added new test helper output

--- src/uTest.hpp
+++ src/uTest.hpp
@@ -76,6 +76,8 @@ int curTestCaseAdded = 0; //0: pass, 1: fail
 
 #define ADD_CUSTOM_TEST_RESULT(bool_value) EXPECT_EQUALS(true, bool_value)
 
+#define PRINT_ADDITIONAL_TEST_INFO(string) OUTPUT_METHOD(String("\t[INFO] ") + String(string))
+
 #define TEST_RESULT_BOOL() testResult
 #define TEST_RESULT_STRING() ((TEST_RESULT_BOOL()) ? String("[PASSED] ") : String("[FAILED] ")) + "in \"" + testComponent + "\" in component \"" + testCase + "\""
 
